087f8e1c3f888ded8f717d9dc7826cf192dbcb60
lucianomariante <51770640+lucianomariante@users.noreply.github.com>
2025-06-04 17:01:28 -0300

    fix imports and trading logic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 import asyncio
-from crypto_bot import main as run_bot   # importa a função main() definida no arquivo novo
+from crypto_bot_mexc import main as run_bot
 
 if __name__ == "__main__":
     asyncio.run(run_bot())
